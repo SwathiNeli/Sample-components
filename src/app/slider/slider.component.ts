91b710dbc3070c786030b1c5ca3a4f92f5fad61a
unknown <swatiui125@gmail.com>
2018-04-10 13:19:27 -0500

    My commits

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -7,23 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
-	Data:Array<any> = [{
-		Title: "Image 1",
-		URL: "assets/images/img_fjords_wide.jpg"
-	},{
-		Title: "Image 2",
-		URL: "assets/images/img_lights_wide.jpg"
-	},{
-		Title: "Image 3",
-		URL: "assets/images/img_mountains_wide.jpg"
-	},{
-		Title: "Image 4",
-		URL: "assets/images/img_nature_wide.jpg"
-	}];
-	CurrentIndex:Number = 0;
-	SlideWidth:Number = 1000;
-	MoveBy:any;
-	NumSlides:Number = this.Data.length;
+  Data:Array<any> = [{
+    Title: "Image 1",
+    URL: "assets/images/img_fjords_wide.jpg"
+  },{
+    Title: "Image 2",
+    URL: "assets/images/img_lights_wide.jpg"
+  },{
+    Title: "Image 3",
+    URL: "assets/images/img_mountains_wide.jpg"
+  },{
+    Title: "Image 4",
+    URL: "assets/images/img_nature_wide.jpg"
+  }];
+  CurrentIndex:any = 0;
+  SlideWidth:any = 1000;
+  MoveBy:any;
+  NumSlides:any = this.Data.length;
 
   constructor() { }
 
@@ -31,22 +31,22 @@ export class SliderComponent implements OnInit {
   }
 
   GoBack( event ){
-  	event.preventDefault();
-  	if ( this.CurrentIndex >0 ) { this.CurrentIndex--};
-  	let MoveBy = this.SlideWidth * this.CurrentIndex;
-  	this.MoveBy = "translateX(-"+ MoveBy +"px)";
+    event.preventDefault();
+    if ( this.CurrentIndex >0 ) { this.CurrentIndex--};
+    let MoveByPixels = this.SlideWidth * this.CurrentIndex;
+    this.MoveBy = "translateX(-"+ MoveByPixels +"px)";
   }
   MoveNext( event ){
-  	event.preventDefault();
-  	if ( this.CurrentIndex < this.NumSlides-1) { this.CurrentIndex++};
-  	let MoveBy = this.SlideWidth * this.CurrentIndex;
-  	this.MoveBy = "translateX(-"+ MoveBy +"px)";
+    event.preventDefault();
+    if ( this.CurrentIndex < this.NumSlides-1) { this.CurrentIndex++};
+    let MoveByPixels = this.SlideWidth * this.CurrentIndex;
+    this.MoveBy = "translateX(-"+ MoveByPixels +"px)";
   }
   MoveTo( _index, _event ){
-  	_event.preventDefault();
-  	this.CurrentIndex = _index || 0;
-  	let MoveBy = this.SlideWidth * this.CurrentIndex;
-  	this.MoveBy = "translateX(-"+ MoveBy +"px)";
+    _event.preventDefault();
+    this.CurrentIndex = _index || 0;
+    let MoveByPixels = this.SlideWidth * this.CurrentIndex;
+    this.MoveBy = "translateX(-"+ MoveByPixels +"px)";
   }
 
 }
